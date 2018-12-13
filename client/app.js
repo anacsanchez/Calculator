3037a9e7dd3d7c656f4a9569cd38b084108aaba3
Ana C Sanchez <ana.sanchez6547@gmail.com>
2018-12-12 22:53:25 -0500

    fixed styling and title

--- client/app.js
+++ client/app.js
@@ -3,7 +3,10 @@ import { Calculator } from './components';
 
 const App = () => {
   return (
+    <div>
+    <div id="title">Calculator</div>
     <Calculator />
+    </div>
   )
 }
 export default App;
